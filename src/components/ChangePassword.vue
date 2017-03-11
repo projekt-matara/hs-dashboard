5c1bee8de1e0d8cb37fcba567b548257bf4c95f2
Alex Allen <alex@kihon.co>
2017-03-10 19:42:31 -0600

    changePassword revamped and now works

--- src/components/ChangePassword.vue
+++ src/components/ChangePassword.vue
@@ -6,21 +6,34 @@ main.mdl-layout__content.mdl-color--grey-100
 	.mdl-cell--12-col
 		#edit_email.demo-card-event.mdl-card.mdl-shadow--2dp
 			.mdl-card__title.mdl-card--expand
-				h2.mdl-card__title-text Change password
+				h2.mdl-card__title-text Change Password
 			.mdl-card__supporting-text
-				p To change your password, click the button below then check your email. From there you will then be able to change your password.
-			.mdl-card__actions
-						button(
-						class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" 
-						@click="changePassword") Change Password
+				form(@submit.prevent="onSubmit()", @submit="changePassword()")
+					.mdl-textfield.mdl-js-textfield.mdl-textfield--floating-label
+						input(class="mdl-textfield__input", type="password", name="oldpassword", v-model="oldpassword")
+						label(class="mdl-textfield__label", for="oldpassword") Old Password
+					.mdl-textfield.mdl-js-textfield.mdl-textfield--floating-label
+						input(class="mdl-textfield__input" type="password", name="newpassword", v-model="newpassword")
+						label(class="mdl-textfield__label" for="newpassword") New Password
+					.mdl-textfield.mdl-js-textfield.mdl-textfield--floating-label
+						input(class="mdl-textfield__input" type="password", name="confirmpassword", v-model="confirmpassword")
+						label(class="mdl-textfield__label" for="confirmpassword") Confirm Password
+					.mdl-card__actions
+						input(type="submit" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" value="Change Password")
+		div(v-if="isError")
+			.mdl-card.mdl-shadow--2dp.error-message#error-login
+				.mdl-card__title.mdl-card--expand
+					h1.mdl-card__title-text Error
+				.mdl-card__supporting-text
+					p {{error}}
 		div(
 		class="demo-card-event mdl-card mdl-shadow--2dp"
 		id="password_success"
 		v-if="changeSuccess")
 			.mdl-card__title.mdl-card--expand
-				h2.mdl-card__title-text Email sent
+				h2.mdl-card__title-text Success!
 			.mdl-card__supporting-text
-				p Check your email to change your password.
+				p Your password has successfully been changed.
 			.mdl-card__actions
 						button(
 						class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" 
@@ -63,9 +76,12 @@ export default {
 
 	data () {
 		return {
-			password: '',
-			confirmPassword: '',
-			changeSuccess: false
+			oldpassword: '',
+			newpassword: '',
+			confirmpassword: '',
+			changeSuccess: false,
+			isError: false,
+			error: ''
 		}
 	},
 
@@ -78,19 +94,36 @@ export default {
 
 	methods: {
 		changePassword () {
-			this
-			.$http
-			.post('https://halfstak.auth0.com/dbconnections/change_password', {
-				email: this.userEmail,
-				clientId: 'LXrECoaQZHEP9TAe8ceisjDd0q49uDDI',
-				connection: 'Username-Password-Authentication'
-			})
-			.then((response) => {
-				this.changeSuccess = true
-			})
-			.catch((err) => {
-				console.log(err)
-			})
+			if (this.newpassword === this.confirmpassword) {
+				const jwtHeader = {'Authorization': 'Bearer ' + localStorage.getItem('idToken')},
+							oldPassword = this.oldpassword,
+							newPassword = this.newpassword,
+							confirmPassword = this.confirmpassword,
+							userId = this.userId,
+							payload = {oldPassword, newPassword, confirmPassword, userId}
+				this
+				.$http
+				.put('http://localhost:3000/changepassword', payload, {headers: jwtHeader})
+				.then((response) => {
+					if (response.data.message === 'Password change successful.') {
+						this.changeSuccess = true
+					} else {
+						throw new Error('Ah crap, there has been an error in changing your password. :-(')
+					}
+				})
+				.catch((err) => {
+					if (err.message) {
+						this.isError = true
+						this.error = err.message
+					} else {
+						this.isError = true
+						this.error = err
+					}
+				})
+			} else {
+				this.isError = true
+				this.error = 'New Password and Confirm Password do not match. Please try entering them again.'
+			}
 		},
 		backToAccount () {
 			router.go('account')
